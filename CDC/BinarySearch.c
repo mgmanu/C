2c6b05846c514a4addef9862bf93f000830b2a71
Manish G <lmanish2007@gmail.com>
2026-09-26 23:55:46 +0530

    BinarySearch

---
+++ CDC/BinarySearch.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+int main(){
+    int low=0;
+    int high=n-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(arr[mid]>target) high=mid-1;
+        else if(arr[mid]<target) low=mid+1;
+        else mid=i;
+    }
+    i++
+}
